98e2a278c3b3825d18c9f572630f8428af1d0851
Mahdi Bagheri <mahdibagheri@mac2021.local>
2023-02-14 13:00:47 +0330

    finish Models

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext as _
 
 
 class Category(models.Model):
-    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE()),
+    parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE),
     title = models.CharField(verbose_name=_('Title'), max_length=50)
     description = models.TextField(verbose_name=_('Description'), blank=True)
     avatar = models.ImageField(verbose_name=_('Avatar'), blank=True, upload_to='categories')
@@ -22,7 +22,7 @@ class Product(models.Model):
     description = models.TextField(_('Description'), blank=True)
     avatar = models.ImageField(_('Avatar'), blank=True, upload_to='products/')
     is_enable = models.BooleanField(_('Is Enable'), default=True)
-    categories = models.ManyToManyField(_('Category'), verbose_name=_('Categories'), blank=True)
+    categories = models.ManyToManyField('Category', verbose_name=_('Categories'), blank=True)
     created_time = models.DateTimeField(verbose_name=_('Created Time'), auto_now_add=True)
     updated_time = models.DateTimeField(verbose_name=_('Updated Time'), auto_now=True)
 
